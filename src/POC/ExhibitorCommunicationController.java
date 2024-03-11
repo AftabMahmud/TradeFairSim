358a715c774556a191041a594345159284fdfea0
AftabMahmud <2022058@iub.edu.bd>
2024-03-12 00:03:10 +0600

    aftab th dsa 2nd

--- src/POC/ExhibitorCommunicationController.java
+++ src/POC/ExhibitorCommunicationController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package manager;
+package POC;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author Asus
  */
-public class POCnoticeboardController implements Initializable {
+public class ExhibitorCommunicationController implements Initializable {
 
     /**
      * Initializes the controller class.
